43d8d9bf21169272393a0c34ab3d56b88e85161a
Thierry Bela Nanga <bntfr@yahoo.fr>
2010-05-01 12:07:29 +0100

    update for the forge

--- Source/Tab.Plugins.Move.js
+++ Source/Tab.Plugins.Move.js
@@ -7,7 +7,7 @@ copyright: Copyright (c) 2008 Thierry Bela
 authors: [Thierry Bela]
 
 requires: 
-  tab:0.1.3.2: 
+  tab:0.1.3.3: 
   - Tab
 provides: [Tab.plugins.Move]
 ...
--- Source/Tab.Plugins.Stack.js
+++ Source/Tab.Plugins.Stack.js
@@ -8,7 +8,7 @@ copyright: Copyright (c) 2008 Thierry Bela
 authors: [Thierry Bela]
 
 requires: 
-  tab:0.1.3.2: 
+  tab:0.1.3.3: 
   - Tab
 provides: [Tab.plugins.Stack]
 ...
