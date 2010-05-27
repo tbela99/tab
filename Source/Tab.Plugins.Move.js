07b355f9fe2dbde3b77850e37a806863833536b8
Thierry Bela Nanga <bntfr@yahoo.fr>
2010-05-27 12:48:23 +0100

    removing redondant code

--- Source/Tab.Plugins.Move.js
+++ Source/Tab.Plugins.Move.js
@@ -32,34 +32,48 @@ provides: [Tab.plugins.Move]
 		
 			this.options = $merge(this.options, options);
 			
-			this.panels = panels.map(function (el) {
-			
-				
-				return el.setStyles({position: 'absolute', display: 'block'});
-				
-			});
+			this.panels = panels.map(function (el) { return el.setStyles({position: 'absolute', display: 'block'}) });
 			
 			this.fx = new Fx.Elements(panels, $merge(this.fx, fx));
 			
-			var pos = 0;
-			
-			this.panels.each(function(panel) {
-			
-				panel.setStyles({position: 'absolute', display: 'block'});
+			this.reorder(0, 1).container = panels[0].getParent().setStyles({overflow: 'hidden', position: 'relative', width: panels[0].offsetWidth, height: panels[0].offsetHeight})
+		},
+		
+		reorder: function (offset, direction) {
+		
+			var pos = 0,
+				i,
+				options = this.options,
+				panels = this.panels,
+				length = panels.length,
+				horizontal = options.mode == 'horizontal',
+				side = horizontal ? 'offsetWidth' : 'offsetHeight';
 				
-				if(this.options.mode == 'horizontal') {
+			//rtl
+			if(direction == -1) {
+			
+				for(i = length; i > 0; i--) {
+			
+					index = (i + offset + length) % length;
+					panel = panels[index];
 					
-					panel.setStyle('left', pos);
-					pos += panel.offsetWidth
-				} else {
-				
-					panel.setStyles({left: 0, top: pos});
-					pos += panel.offsetHeight
+					if(horizontal) panel.setStyle('left', pos);
+					else panel.setStyles({left: 0, top: pos});
+					pos -= panel[side];
 				}
 				
-			}, this);
+				//ltr
+			} else if(direction == 1) for(i = 0; i < length; i++) {
 			
-			this.container = panels[0].getParent().setStyles({overflow: 'hidden', position: 'relative', width: panels[0].offsetWidth, height: panels[0].offsetHeight})
+				index = (i + offset + length) % length;
+				panel = panels[index];				
+				
+				if(horizontal) panel.setStyle('left', pos);
+				else panel.setStyles({left: 0, top: pos});
+				pos += panel[side];
+			}
+			
+			return this
 		},
 		
 		move: function (newTab, curTab, newIndex, oldIndex, direction) {
@@ -67,47 +81,16 @@ provides: [Tab.plugins.Move]
 			this.fx.cancel();
 			var obj = {}, 
 				options = this.options,
-				panels = this.panels,
-				length = panels.length,
 				horizontal = options.mode == 'horizontal',
-				property = horizontal ? 'offsetLeft' : 'offsetTop', 
-				side = horizontal ? 'offsetWidth' : 'offsetHeight', 
+				property = horizontal ? 'offsetLeft' : 'offsetTop',  
 				offset,
-				//morph,
-				opacity = options.opacity || .7,
-				pos = 0,
-				i;
+				opacity = options.opacity || .7;
 			
-			if(oldIndex != undefined && options.circular)  {
-					
-				//rtl
-				if(direction == -1) {
-				
-					for(i = length; i > 0; i--) {
-				
-						index = (i + oldIndex + length) % length;
-						panel = panels[index];
-						
-						if(horizontal) panel.setStyle('left', pos);
-						else panel.setStyles({left: 0, top: pos});
-						pos -= panel[side];
-					}
-					
-					//ltr
-				} else if(direction == 1) for(i = 0; i < length; i++) {
-				
-					index = (i + oldIndex + length) % length;
-					panel = panels[index];				
-					
-					if(horizontal) panel.setStyle('left', pos);
-					else panel.setStyles({left: 0, top: pos});
-					pos += panel[side];
-				}
-			}
+			if(oldIndex != undefined && options.circular) this.reorder(oldIndex, direction);
 			
 			offset = newTab[property];
 		
-			panels.each(function(panel, index) {
+			this.panels.each(function(panel, index) {
 			
 				obj[index] = horizontal ? {opacity: opacity, left:[panel[property], panel[property] - offset]} : {opacity: opacity, top:[panel[property], panel[property] - offset]};
 		
