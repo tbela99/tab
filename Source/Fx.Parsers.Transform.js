52f3ae92da7f2a6583e985d7ba3aa10b71118d7e
Thierry Bela <bnanga@gmail.com>
2011-11-08 10:09:39 +0100

    .

--- Source/Fx.Parsers.Transform.js
+++ Source/Fx.Parsers.Transform.js
@@ -71,7 +71,8 @@ provides: [Fx.CSS.Parsers.Transform]
 var deg = ['skew', 'rotate'],
 	px = ['translate'],
 	generics = ['scale'],
-	coordinates = ['X', 'Y', 'Z'];
+	coordinates = ['X', 'Y', 'Z'], 
+	number = '\\s*([-+]?([0-9]*\.)?[0-9]+(e[+-]?[0-9]+)?)';
 
 	px = px.concat(coordinates.map(function (side) { return px[0] + side }));
 	generics = generics.concat(coordinates.map(function (side) { return generics[0] + side }));
@@ -94,11 +95,22 @@ var deg = ['skew', 'rotate'],
 
 				if(!value) return false;
 				
-				var transform = {}, match;
+				var transform = {}, 
+					match;
+				
+				if((match = value.match(new RegExp('translate3d\\((' + number + ')(px)?\\s*,\\s*('+ number + ')(px)?\\s*,\\s*(' + number + ')(px)?\\s*\\)')))) {
+				
+					transform.translate3d = [parseFloat(match[1]), parseFloat(match[6]), parseFloat(match[12])]
+				}
+
+				if((match = value.match(new RegExp('rotate3d\\(\\s*(' + number + ')\\s*,\\s*('+ number + ')\\s*,\\s*(' + number + ')\\s*,\\s*(' + number + ')(deg|rad)?\\s*\\)')))) {
+				
+					transform.rotate3d = [parseFloat(match[1]), parseFloat(match[5]), parseFloat(match[9]), match[17] == 'rad' ? parseFloat(match[13]).toDeg() : parseFloat(match[13])]
+				}
 
 				if(px.every(function (t) {
 
-					if((match = value.match(new RegExp(t + '\\(\\s*([-+]?([0-9]*\.)?[0-9]+(e[+-]?[0-9]+)?)(px)?\\s*(,\\s*([-+]?([0-9]*\.)?[0-9]+(e[+-]?[0-9]+)?)(px)?\\s*)?\\)', 'i')))) {
+					if((match = value.match(new RegExp(t + '\\(' + number + '(px)?\\s*(,' + number + '(px)?\\s*)?\\)', 'i')))) {
 
 						var x = parseFloat(match[1]),
 							y = parseFloat(match[6]);
@@ -123,7 +135,7 @@ var deg = ['skew', 'rotate'],
 					//5 - number defined
 					//6 - number
 					//9 - unit
-					if((match = value.match(new RegExp(t + '\\(\\s*([-+]?([0-9]*\.)?[0-9]+(e[+-]?[0-9]+)?)(deg|rad)?\\s*(,\\s*([-+]?([0-9]*\.)?[0-9]+(e[+-]?[0-9]+)?)(deg|rad)?)?\\s*\\)', 'i')))) {
+					if((match = value.match(new RegExp(t + '\\(' + number + '(deg|rad)?\\s*(,' + number + '(deg|rad)?)?\\s*\\)', 'i')))) {
 
 						var x = parseFloat(match[1]),
 							y = parseFloat(match[6]);
@@ -178,7 +190,7 @@ var deg = ['skew', 'rotate'],
 			serve: function(transform){
 
 				var style = '';
-
+				
 				deg.each(function (t) {
 
 					if(transform[t] != null) {
@@ -191,6 +203,10 @@ var deg = ['skew', 'rotate'],
 				px.each(function (t) { if(transform[t] != null) style += t + '(' + Array.from(transform[t]).map(function (value) { return value + 'px' }) + ') ' });
 				generics.each(function (t) { if(transform[t] != null) style += t + '(' + transform[t] + ') ' });
 
+				if(transform.translate3d) style += ' translate3d(' + transform.translate3d[0]+ 'px,' + transform.translate3d[1]+ 'px,' + transform.translate3d[2]+ 'px)';
+				if(transform.rotate3d) style += ' rotate3d(' + transform.rotate3d[0]+ ',' + transform.rotate3d[1]+ ',' + transform.rotate3d[2]+ ', ' + transform.rotate3d[3] + 'deg)';
+				
+				//console.log(style)
 				return style
 			}
 		}
